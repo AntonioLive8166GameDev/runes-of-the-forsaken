280d8e754fca1cd1efb74892abb28464d76d8a48
AntonioLive8166 <jhernandezaceves33@gmail.com>
2025-03-07 10:19:24 -0600

    Corrección de errores
    Se corrigió el mapeo de controles para mandos y se cambió el idioma de los comentarios existentes en el código al español.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -7,7 +7,7 @@ func _process(_delta: float) -> void:
 	motion()
 
 
-# Makes player move changing the CharacterBody2D velocity property to the input of player * the speed
+# Hace que el jugador se mueva cambiando la propiedad velocity del nodo CharacterBody2D (Player).
 func motion() -> void:
 	velocity.x = GLOBAL.get_axis().x * speed
 	velocity.y = GLOBAL.get_axis().y * -speed
--- scripts/autoloads/global.gd
+++ scripts/autoloads/global.gd
@@ -1,11 +1,11 @@
 extends Node
 
-# Stores the input from user in two axis
+# Variable para guardar el input del usuario.
 var axis : Vector2
 
 
-# Gets the input axis
+# Obtener los ejes del input mediante operaciónes aritméticas simples.
 func get_axis() -> Vector2:
 	axis.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	axis.y = int(Input.is_action_pressed("up")) - int(Input.is_action_pressed("down"))
-	return axis.normalized() # It's normalized to avoid increased speed when there's input on both vectors
+	return axis.normalized() # Se devuelve el valor normalizado para evitar que el movimiento en diagonal sea más rápido.
