e4cb682dddef03013c07b4c9e0d024567151c5c7
AntonioLive8166 <jhernandezaceves33@gmail.com>
2025-04-14 20:50:18 -0600

    Documentación  y mejoras

--- scenes/levels/arcade_level.gd
+++ scenes/levels/arcade_level.gd
@@ -4,12 +4,25 @@ extends Node2D
 ##
 ## Esta clase ofrece la posibilidad de eliminarse del árbol de escenas emitiendo antes una señal
 ## al llamar su método [method ArcadeLevel.end_level]. Esto permite tomar acciones cuando
-## el nivel finalice.
+## el nivel finalice. También añade al jugador al árbol de escenas y cambia su posición. [br]
+##
+## [color=red]Advertencia:[/color] Se requiere un nodo [u]PlayerStartPos[/u] de clase [Marker2D]
+## como hijo del nodo principal. El prámetro [param position] de este nodo se utiliza para establecer
+## la posición del [b]jugador[/b]; si el nodo no existe, ocurrirá un error.
 ##
 ## @experimental: Esta clase aún está siendo probada, por lo que su funcionalidad es limitada.
 
 ## Señal emitida al llamar el método [method ArcadeLevel.end_level].
 signal level_ended
+
+## Guarda una instancia del jugador para su manejo.
+var _player_instance = preload("res://scenes/player/player.tscn").instantiate()
+
+
+func _ready() -> void:
+	# Añade el jugador a la escena y cambia su posición.
+	add_child(_player_instance)
+	$Player.position = $PlayerStartPos.position
 
 
 ## Elimina la escena actual del árbol de escenas y emite [signal ArcadeLevel.level_ended].
--- scenes/levels/test_main_scene.gd
+++ scenes/levels/test_main_scene.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 # TODO: Make an automatic preload system based on file system and not manually colocations.
 ## Contiene las escenas de los niveles precargados. Sirven como referencia.
+
 @export var levels : Array[PackedScene] = [
 	preload("res://scenes/levels/test_level.tscn"),
 	preload("res://scenes/levels/test_level1.tscn"),
@@ -14,6 +15,7 @@ var _levels : Array[PackedScene] = levels.duplicate()
 func _ready() -> void:
 	# "Barajamos" los elementos de _levels para mejorar la aleatoriedad.
 	_levels.shuffle()
+	select_random_level()
 
 
 ## Selecciona e instancia una escena de las disponibles en [u]_levels[/u] para posteriormente agregarla al árbol de escenas.
@@ -23,10 +25,15 @@ func select_random_level() -> void:
 		_levels = levels.duplicate()
 		_levels.shuffle()
 	
-	## Selecciona un elemento de _levels, se guarda una instancia en last_level y luego se elimina de _levels para garantizar
-	## que no pueda ser elegido consecutivamente.
+	# Selecciona un elemento de _levels, se guarda una instancia en last_level y luego se elimina de _levels para garantizar
+	# que no pueda ser elegido consecutivamente.
 	var last_level = _levels.pop_front().instantiate()
 	add_child(last_level)
 	# Ahora que uno de los niveles/escenarios ha sido añadido al árbol de escenas, podemos conectar su señal "level_ended" con
 	# el método que necesitemos (con fines de depuración, actualmente la conecté a esta misma función).
-	last_level.level_ended.connect(select_random_level)
+	last_level.level_ended.connect(_on_arcade_level_level_ended)
+
+
+## Una señal proveniente del nivel seleccionado se cnonecta con esta función cuando éste finaliza.
+func _on_arcade_level_level_ended() -> void:
+	pass
