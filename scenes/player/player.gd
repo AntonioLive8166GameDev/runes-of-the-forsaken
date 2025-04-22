2696d6f77a1527421b9332baaa659214ea69070a
AntonioLive8166 <jhernandezaceves33@gmail.com>
2025-04-22 12:46:08 -0600

    Prototipo de función de muerte y documentación.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,10 +6,11 @@ extends CharacterBody2D
 ## @experimental: Aún faltan algunas estadísticas y métodos.
 
 @export_group("Estadísticas")
-@export var speed : int = 100
-@export var hp : int = 100
-@export var defense : int = 100
-@export var atk : int = 50
+@export var speed : int = 100 ## Velocidad del jugador.
+@export var hp : int = 100 ## Vida del jugador.
+@export var defense : int = 100 ## Defensa o resistencia ante ataques del jugador.
+@export var atk : int = 50 ## Multiplicador de daño de ataque.
+@export var invulnerable : bool = false ## Si el jugador puede recibir daño o no.
 
 
 func _process(_delta: float) -> void:
@@ -21,6 +22,18 @@ func motion() -> void:
 	velocity.x = GLOBAL.get_axis().x * speed
 	velocity.y = GLOBAL.get_axis().y * -speed
 	move_and_slide()
+
+
+func take_damage(damage : int):
+	# TODO: Programar disminución de daño recibido ante ataques (defensa).
+	hp -= damage
+	if hp <= 0:
+		die()
+
+
+func die():
+	# TODO: Programar la morision.
+	pass
 
 
 # INFO: BasicSword fue añadido temporalmente como hijo, pero debería cambiarse esto al implementar más armas.
