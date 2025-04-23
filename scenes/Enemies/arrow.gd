459a95623e7db6ece4b9f9e6e8bb4d785443765e
AntonioLive8166 <jhernandezaceves33@gmail.com>
2025-04-22 23:41:25 -0600

    Se creó la clase Arrow.

---
+++ scenes/Enemies/arrow.gd
@@ -0,0 +1,17 @@
+class_name Arrow
+extends RigidBody2D
+## @experimental
+
+signal damage_player(amount)
+
+@export var atk : int = 20
+@export var speed : int = 200
+
+
+func _ready() -> void:
+	look_at(get_global_mouse_position())
+
+
+func _on_player_hit(body: Node) -> void:
+	if body.is_in_group("players"):
+		emit_signal("damage_player", atk)
