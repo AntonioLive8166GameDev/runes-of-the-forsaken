e6e122659eb3a66d3cdba1b7880f25822bc45f9b
AntonioLive8166 <jhernandezaceves33@gmail.com>
2025-05-11 09:41:46 -0600

    Documentación

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -6,21 +6,25 @@ func _ready() -> void:
 	load_settings()
 
 
+## Carga los ajustes guardados con el singletone SETTINGS.
 func load_settings() -> void:
+	# Música y SFX.
 	AudioServer.set_bus_mute(1, SETTINGS.settings.music_muted)
 	$Panel/VBoxContainer/Music.button_pressed = not SETTINGS.settings.music_muted
 	AudioServer.set_bus_mute(2, SETTINGS.settings.sfx_muted)
 	$Panel/VBoxContainer/SFX.button_pressed = not SETTINGS.settings.sfx_muted
 	
+	# Idioma.
 	match SETTINGS.settings.language:
-		"es_MX":
+		"es", "es_MX":
 			$Panel/VBoxContainer/LanguagePopup.select(0)
 			TranslationServer.set_locale("es")
-		"en_US":
+		"en", "en_US":
 			$Panel/VBoxContainer/LanguagePopup.select(1)
 			TranslationServer.set_locale("en")
 
 
+## Interruptor de música.
 func _on_music_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		AudioServer.set_bus_mute(1, false)
@@ -29,9 +33,10 @@ func _on_music_toggled(toggled_on: bool) -> void:
 		AudioServer.set_bus_mute(1, true)
 		SETTINGS.settings.music_muted = true
 	
-	SETTINGS.save_file()
+	SETTINGS.save_settings()
 
 
+## Interruptor de efectos de sonido.
 func _on_sfx_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		AudioServer.set_bus_mute(2, false)
@@ -40,19 +45,20 @@ func _on_sfx_toggled(toggled_on: bool) -> void:
 		AudioServer.set_bus_mute(2, true)
 		SETTINGS.settings.sfx_muted = true
 	
-	SETTINGS.save_file()
+	SETTINGS.save_settings()
 
 
+## Opción de idioma.
 func _on_option_button_item_selected(index: int) -> void:
 	match index:
 		0:
 			TranslationServer.set_locale("es")
-			SETTINGS.settings.language = "es_MX"
+			SETTINGS.settings.language = "es"
 		1:
 			TranslationServer.set_locale("en")
-			SETTINGS.settings.language = "en_US"
+			SETTINGS.settings.language = "en"
 	
-	SETTINGS.save_file()
+	SETTINGS.save_settings()
 
 
 func _on_close_pressed() -> void:
--- scripts/autoloads/settings.gd
+++ scripts/autoloads/settings.gd
@@ -1,27 +1,30 @@
 extends Node
+## Save system for settings.
 
+## File path
 const FILENAME: String = "user://settings.dat"
 var settings = {
-	"music_muted" : false,
+	"music_muted" : false, 
 	"sfx_muted" : false,
 	"language" : TranslationServer.get_locale()
 }
 
 
 func _ready() -> void:
-	load_file()
+	load_settings()
 
-
-func load_file() -> void:
+## Sets [param settings] with the sotored data.
+func load_settings() -> void:
 	var file = FileAccess.open(FILENAME, FileAccess.READ)
 	if file == null:
-		save_file()
+		save_settings()
 	else:
 		settings = file.get_var()
 	file = null
 
 
-func save_file() -> void:
+## Stores [param settings] in a file in [constant SETTINGS.FILENAME].
+func save_settings() -> void:
 	var file = FileAccess.open(FILENAME, FileAccess.WRITE)
 	if file != null:
 		file.store_var(settings)
